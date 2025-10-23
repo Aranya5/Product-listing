64d2e79d2ab471c7480e12269bd4d7c3a91ffbe1
Aranya5 <aranyabasu5@gmail.com>
2025-10-23 20:07:29 +0530

    backend working

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,7 @@ import ProductRoutes from './routes/product.route.js'
 dotenv.config();
 
 const app = express();
+const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 
@@ -13,8 +14,8 @@ app.use('/api/products', ProductRoutes);
 
 
 
-app.listen(3000, () => {
+app.listen(PORT, () => {
   connectDB();
-  console.log('Server is running on port 3000');
+  console.log('Server is running on port:' + PORT);
 });
 
